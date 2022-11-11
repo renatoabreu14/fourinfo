a4dda0d77cad484b3be505b0f9889eb87ae2900b
renatoabreu14 <renato.abreu.info@gmail.com>
2022-11-11 19:53:14 -0300

    Project created

---
+++ backend/src/main/java/com/pw12022/fourinfo/FourinfoApplication.java
@@ -0,0 +1,13 @@
+package com.pw12022.fourinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourinfoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pw12022/fourinfo/FourinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pw12022.fourinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
